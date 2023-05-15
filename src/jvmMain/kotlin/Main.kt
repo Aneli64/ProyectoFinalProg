9f0f91cd244b6db553248aacacfc8a5651abcc53
Aneli <eliandeniz27@gmail.com>
2023-05-15 19:45:54 +0200

    mirar select pq no va

--- src/jvmMain/kotlin/Main.kt
+++ src/jvmMain/kotlin/Main.kt
@@ -1,4 +1,5 @@
 import androidx.compose.desktop.ui.tooling.preview.Preview
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
@@ -12,6 +13,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -38,8 +40,11 @@ fun BookApp() {
         file.forEachLine {
             listaLibros.add(
                 Libro(
-                    it.split(",")[0], it.split(",")[1].toDouble(), it.split(",")[2],
-                    it.split(",")[3], it.split(",")[4].toDouble()
+                    it.split(",")[0],
+                    it.split(",")[1].toDouble(),
+                    it.split(",")[2],
+                    it.split(",")[3],
+                    it.split(",")[4].toDouble()
                 )
             )
         }
@@ -51,10 +56,63 @@ fun BookApp() {
         var autor by remember { mutableStateOf("") }
         var fechaLanz by remember { mutableStateOf("") }
         var precio by remember { mutableStateOf("") }
-        var paginas by remember { mutableStateOf(0) }
+        var user by remember { mutableStateOf("") }
+        var password by remember { mutableStateOf("") }
+        var paginas by remember { mutableStateOf(20) }
+
+        Image(
+            painter = painterResource("bibliotec.jpg"),
+            contentDescription = "imagenBiblioteca",
+            modifier = Modifier.fillMaxSize()
+        )
 
         when (paginas) {
             //Caja 0 (Inicio)
+            20 -> {
+                Column {
+                    Row {
+                        Text("LOGIN", color = Color.Red, fontSize = 40.sp, textAlign = TextAlign.Center)
+                    }
+                    Row {
+                        TextField(value = user, onValueChange = { user = it }, label = { Text(text = "User") })
+                    }
+                    Row {
+                        TextField(value = password, onValueChange = { password = it }, label = { Text(text = "Password") })
+                    }
+                    Row {
+                        Button(onClick = {
+
+                            paginas = 13
+
+                        }) {
+                            Text("Aceptar")
+                        }
+                    }
+                }
+            }
+            13 -> {
+                //usuarios y contraseñas de la BD
+                val statement = conn.createStatement()
+                val resultado = statement.executeQuery("SELECT USUARIO, PASSWORD FROM USERS")
+                Column {
+                    Row {
+                        val lista = mutableListOf<String>()
+                        while(resultado.next()) {
+                            val user = resultado.getString("USUARIO")
+                            //val pw = resultado.getString("PASSWORD")
+                            lista.add(user)
+                        }
+                        Text(lista.toString())
+                    }
+                    Row {
+                        Button(onClick = {
+                            paginas = 0
+                        }) {
+                            Text("volver al inicio")
+                        }
+                    }
+                }
+            }
             0 -> {
                 Box {
                     Column {
@@ -75,6 +133,13 @@ fun BookApp() {
                                 Text("Ir a select")
                             }
                         }
+                        Row {
+                            Button(onClick = {
+                                paginas = 3
+                            }) {
+                                Text("Inf libros")
+                            }
+                        }
                     }
                 }
 
@@ -83,30 +148,19 @@ fun BookApp() {
             1 -> {
                 Box {
                     Column {
-                        TextField(value = titulo,
-                            onValueChange = { titulo = it },
-                            label = { Text(text = "Titulo") }
-                        )
+                        TextField(value = titulo, onValueChange = { titulo = it }, label = { Text(text = "Titulo") })
 
                         TextField(value = calificacion,
                             onValueChange = { calificacion = it },
-                            label = { Text(text = "Calificacion") }
-                        )
+                            label = { Text(text = "Calificacion") })
 
-                        TextField(value = autor,
-                            onValueChange = { autor = it },
-                            label = { Text(text = "Autor") }
-                        )
+                        TextField(value = autor, onValueChange = { autor = it }, label = { Text(text = "Autor") })
 
                         TextField(value = fechaLanz,
                             onValueChange = { fechaLanz = it },
-                            label = { Text(text = "Fecha de lanzamiento") }
-                        )
+                            label = { Text(text = "Fecha de lanzamiento") })
 
-                        TextField(value = precio,
-                            onValueChange = { precio = it },
-                            label = { Text(text = "Precio") }
-                        )
+                        TextField(value = precio, onValueChange = { precio = it }, label = { Text(text = "Precio") })
 
                         Row {
                             Button(onClick = {
@@ -135,14 +189,6 @@ fun BookApp() {
                                 Text("Add Book")
                             }
                             Button(onClick = {
-                                println(listaLibros.toList())
-                                /*var texto = ""
-                                listaLibros.forEach { texto += "$it\n" }
-                                Text(texto)*/
-                            }) {
-                                Text("Show Books")
-                            }
-                            Button(onClick = {
                                 paginas = 0
                             }) {
                                 Text("volver al inicio")
@@ -158,11 +204,76 @@ fun BookApp() {
 
             //Select de libros
             2 -> {
-                LazyColumn {
-                    LazyRow {//tendriamos que sacar los datos de select primero????
-                        Text(tabla(listaLibros))
+                Box {
+                    LazyColumn {
+                        item {
+                            Row {
+                                Text(tabla(listaLibros))
+                            }
+                            Row {
+                                Button(onClick = {
+                                    paginas = 0
+                                }) {
+                                    Text("volver al inicio")
+                                }
+                            }
+                        }
                     }
-                    LazyRow {
+                }
+            }
+
+            3 -> {
+                Box {
+                    Column {
+                        Row {
+                            Text("Informacion varia acerca de los libros")
+                        }
+                        Row {
+                            Button(onClick = {
+                                paginas = 4
+                            }) {
+                                Text("Libro con mayor calif")
+                            }
+                        }
+                        Row {
+                            Button(onClick = {
+                                paginas = 5
+                            }) {
+                                Text("Libro con menor calif")
+                            }
+                        }
+                        Row {
+                            Button(onClick = {
+                                paginas = 0
+                            }) {
+                                Text("volver al inicio")
+                            }
+                        }
+                    }
+                }
+            }
+
+            4 -> {
+                Column {
+                    Row {
+                        Text(maxLibro(listaLibros))
+                    }
+                    Row {
+                        Button(onClick = {
+                            paginas = 0
+                        }) {
+                            Text("volver al inicio")
+                        }
+                    }
+                }
+            }
+
+            5 -> {
+                Column {
+                    Row {
+                        Text(minLibro(listaLibros))
+                    }
+                    Row {
                         Button(onClick = {
                             paginas = 0
                         }) {
@@ -174,18 +285,40 @@ fun BookApp() {
         }
     }
 }
+
 @Composable
 @Preview
-fun tabla(lista: MutableList<Libro>): String {
+fun tabla(lista: MutableList<Libro>): String { //intentar que quede mas bonita
     var texto = ""
-    val cadena1 = "-".repeat(20)
+    val cadena1 = "-".repeat(40)
     lista.forEach {
-        texto +=
-            cadena1 + "\n"
-        "| ${it.titulo} | ${it.calif} | ${it.autor}| ${it.fech_lanz}| ${it.precio} |" + "\n" +
-                cadena1
+        Column {
+            texto += cadena1 + "\n"
+            texto += "| ${it.titulo} | ${it.calif} | ${it.autor}| ${it.fech_lanz}| ${it.precio} |" + "\n"
+            texto += cadena1
+        }
     }
     return texto
+}
+
+@Composable
+@Preview
+fun minLibro(libros: MutableList<Libro>): String{
+    var cadena = ""
+    val libro = libros.sortedBy { it.calif }[0]
+    cadena += "Libro con mayor calificacion" + "\n" + "-".repeat(28) + "\n" +
+            "| ${libro.titulo} | ${libro.calif} | ${libro.autor}| ${libro.fech_lanz}| ${libro.precio} |" + "\n" + "-".repeat(28)
+    return cadena
+}
+//representar tabla mas bonita wei
+@Composable
+@Preview
+fun maxLibro(libros: MutableList<Libro>): String{
+    var cadena = ""
+    val libro = libros.sortedBy { it.calif }[libros.size-1]
+    cadena += "Libro con mayor calificacion" + "\n" + "-".repeat(28) + "\n" +
+            "| ${libro.titulo} | ${libro.calif} | ${libro.autor}| ${libro.fech_lanz}| ${libro.precio} |" + "\n" + "-".repeat(28)
+    return cadena
 }
 
 fun main() = application {
